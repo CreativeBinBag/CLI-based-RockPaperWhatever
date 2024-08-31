d16e40ef2f94c6246f0a552dc6968af878e31753
Nafisa Shamim Rafa <nafisashamim1999@gmail.com>
2024-08-31 15:33:15 +0600

    updated axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 //centralized axios instance to make API requests
 const api = axios.create({
 
-  baseURL:'https://cli-based-rockpaperwhatever-backend.onrender.com',
+  baseURL:'https://cli-based-rockpaperwhateverbackend-fzp2.onrender.com',
   withCredentials: true,
   
 
 });
 
 export default api;
+
