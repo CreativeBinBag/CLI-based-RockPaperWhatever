7fa61be00b7686e0d66aa41cb3f25bbeb473ba1e
Nafisa Shamim Rafa <nafisashamim1999@gmail.com>
2024-08-31 19:23:50 +0600

    fixed file path issue in PrivateRoute.js

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { AuthContext } from "./context/AuthProvider";
-
+import { AuthContext } from "./AuthProvider";
 const PrivateRoute = ({ adminOnly = false }) => {
   const { auth, loading, checkAdmin } = useContext(AuthContext);
 
