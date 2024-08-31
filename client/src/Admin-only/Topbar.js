d90c910ef8f277749763b32e765da522fe5bb5ff
Nafisa Shamim Rafa <nafisashamim1999@gmail.com>
2024-08-31 19:43:07 +0600

    modified topbar.js

--- client/src/Admin-only/Topbar.js
+++ client/src/Admin-only/Topbar.js
@@ -1,17 +1,13 @@
 import {Box} from "@mui/material";
+import { Link } from "react-router-dom";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 const Topbar = () => {
 
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
 
-              <Item
-              title={t('logout')}
-              to="/"
-              icon={<LogoutOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />      
+         <Link to="/logout" > Log Out </Link>
+          
   
     </Box>
   );
