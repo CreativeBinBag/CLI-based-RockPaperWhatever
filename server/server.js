a244d69088dd63d8c2c2e1e64880345cde57dee8
Nafisa Shamim Rafa <nafisashamim1999@gmail.com>
2024-09-01 21:01:37 +0600

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,33 +1,47 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const cookieParser = require('cookie-parser');
-const db = require('./index');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const cookieParser = require('cookie-parser')
+const db = require ('./index')
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { spawn } = require('child_process');
-const WebSocket = require('ws');
+const { sequelize } = require('.');
+const userRoutes = require('./Routes/userRoute')
+const { spawn } = require('child_process'); // Import for spawning child processes
+const WebSocket = require('ws'); // Import for WebSocket
 
-const PORT = process.env.PORT || 10001;
-const app = express();
+
+
+
+//setting up your port
+const PORT = process.env.PORT || 10001
+
+//assigning the variable app to express
+const app = express()
 
 app.use(cors({
-    origin: 'https://cli-based-rockpaperwhatever-nafisa-rafas-projects.vercel.app',
-    credentials: true,
+  origin: 'https://cli-based-rockpaperwhatever-nafisa-rafas-projects.vercel.app', // Use environment variable for client URL
+  credentials: true,
+
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(bodyParser.json());
+//middleware
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(bodyParser.json())
+
 
 db.sequelize.sync().then(() => {
-    console.log("db synced");
+  console.log("db synced")
 });
 
 app.use('/api/users', userRoutes);
 
+// Start the Express server
 const server = app.listen(PORT, '0.0.0.0', () => console.log(`Server is connected on ${PORT}`));
 
+// Initialize WebSocket Server
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
