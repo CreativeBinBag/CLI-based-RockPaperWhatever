5690e4dfb0e7a0983e690074d6106f1f20b5601c
Nafisa Shamim Rafa <nafisashamim1999@gmail.com>
2024-09-02 18:50:51 +0600

    updated private route.js

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "./AuthProvider";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 const PrivateRoute = ({ adminOnly = false }) => {
   const { auth, loading, checkAdmin } = useContext(AuthContext);
 
@@ -11,7 +14,9 @@ const PrivateRoute = ({ adminOnly = false }) => {
   
   if (!auth) {
     console.log('Redirecting to login because not authenticated');
+    toast.error('You need to be logged in to access this page.');
     return <Navigate to="/" />;
+
   }
 
   if (adminOnly && !checkAdmin()) {
